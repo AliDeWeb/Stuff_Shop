791debbf5214483e94b2f07235d2c7e055a62fde
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-02 18:57:47 +0330

    feat: implement password hashing in user service

--- src/main/java/com/github/alideweb/stuffshop/modules/users/UserService.java
+++ src/main/java/com/github/alideweb/stuffshop/modules/users/UserService.java
@@ -1,14 +1,18 @@
 package com.github.alideweb.stuffshop.modules.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     final private UserRepository userRepository;
+    final private PasswordEncoder passwordEncoder;
 
     public UserEntity save(UserEntity user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return userRepository.save(user);
     }
 }
