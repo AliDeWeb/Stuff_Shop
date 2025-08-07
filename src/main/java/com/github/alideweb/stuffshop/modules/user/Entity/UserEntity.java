aa8118e1dbaa9ef964246fed02a7c64cc6c1be90
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-07 23:10:40 +0330

    feat: implement user entity

---
+++ src/main/java/com/github/alideweb/stuffshop/modules/user/Entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.github.alideweb.stuffshop.modules.user.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String name;
+}
