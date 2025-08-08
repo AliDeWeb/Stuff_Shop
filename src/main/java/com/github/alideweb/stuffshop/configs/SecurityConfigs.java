c60936385b9692933aef78c9f7a4107e45cc4ed6
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-08 13:50:50 +0330

    fix: break circular dependency by extracting PasswordEncoder bean

--- src/main/java/com/github/alideweb/stuffshop/configs/SecurityConfigs.java
+++ src/main/java/com/github/alideweb/stuffshop/configs/SecurityConfigs.java
@@ -10,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,13 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfigs {
-    private final UserDetailsService userDetailsService;
     private final JwtFilters jwtFilter;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
