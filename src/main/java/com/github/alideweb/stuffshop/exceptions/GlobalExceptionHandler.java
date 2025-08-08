6e1780c2f447612e853be9e9d01af38b492a66b4
AliDeWeb <AliMoradi0Business@gmail.com>
2025-08-08 22:13:46 +0330

    feat: handle missing query parameters with MissingServletRequestParameterException
    - Returns 400 Bad Request with structured ProblemDetail when a required @RequestParam is missing

--- src/main/java/com/github/alideweb/stuffshop/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/github/alideweb/stuffshop/exceptions/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -48,6 +49,19 @@ public class GlobalExceptionHandler {
         pd.setDetail("Could not parse JSON body");
         pd.setInstance(URI.create(request.getRequestURI()));
         pd.setProperty("code", "ERR_INVALID_JSON");
+
+        return ResponseEntity.badRequest().body(pd);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ProblemDetail> handleMissingRequestParam(MissingServletRequestParameterException ex, HttpServletRequest request) {
+        String detail = String.format("Required query parameter '%s' is missing", ex.getParameterName());
+
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pd.setTitle("Missing request parameter");
+        pd.setDetail(detail);
+        pd.setInstance(URI.create(request.getRequestURI()));
+        pd.setProperty("code", "ERR_MISSING_PARAMETER");
 
         return ResponseEntity.badRequest().body(pd);
     }
